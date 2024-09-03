efe198fd3abbcea3c190875ae127d4fab7358794
Damian Tomczyszyn <damitom@o2.pl>
2024-09-03 10:32:49 +0200

    IoC calculator

--- studentmanager/src/main/java/com/damiantomczyszyn/studentmanager/Calculator.java
+++ studentmanager/src/main/java/com/damiantomczyszyn/studentmanager/Calculator.java
@@ -1,5 +1,8 @@
 package com.damiantomczyszyn.studentmanager;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Calculator {
     public int calculateSum( int a, int b)
     {
--- studentmanager/src/main/java/com/damiantomczyszyn/studentmanager/MySimpleController.java
+++ studentmanager/src/main/java/com/damiantomczyszyn/studentmanager/MySimpleController.java
@@ -1,5 +1,6 @@
 package com.damiantomczyszyn.studentmanager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MySimpleController {
+
+    public final Calculator calculator;
+    public MySimpleController(Calculator calculator){
+        this.calculator = calculator;
+    }
+
+
     @RequestMapping(value={"/","/home"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String home(){
         return "Hello World!";
@@ -18,7 +26,7 @@ public class MySimpleController {
     }
     @RequestMapping("/sum")
     public int sum(@RequestParam("a") int a, @RequestParam("b") int b){
-        return new Calculator().calculateSum(a,b);
+        return calculator.calculateSum(a,b);
     }
 
 }
