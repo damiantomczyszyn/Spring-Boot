45c0ad5a6f608311aa8d614967e3e5c282c676a0
Damian Tomczyszyn <damitom@o2.pl>
2024-09-05 13:52:24 +0200

    disable export of deleting methods

--- todo-app/src/main/java/com/damiantomczyszyn/todo_app/model/TaskRepository.java
+++ todo-app/src/main/java/com/damiantomczyszyn/todo_app/model/TaskRepository.java
@@ -2,8 +2,16 @@ package com.damiantomczyszyn.todo_app.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
+    @Override
+    @RestResource(exported = false)
+    void delete(Task entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Long> longs);
 }
